199f6f9e39c8ffcbcd767403a0af643be54f5beb
antoni <antoni.dev0@gmail.com>
2021-12-04 09:18:56 -0800

    adds pub and sub modules to Driver mod

--- Driver/Consumer.js
+++ Driver/Consumer.js
@@ -1,3 +1,15 @@
 'use strict'
 
-module.exports = ''
+const { Consumer } = require('sqs-consumer')
+const { handleMessage } = require('./Publisher')
+
+// Consume --> Package_Queue
+const app = Consumer.create({
+  queueUrl: 'https://sqs.us-west-2.amazonaws.com/872892230630/Package_Queue',
+  handleMessage: handleMessage
+})
+
+app.on('error', (err) => {console.log('*** err ', err)})
+app.on('processing_error', (err) => { console.log('*** err ', err)})
+
+module.exports = { app }
--- Driver/Publisher.js
+++ Driver/Publisher.js
@@ -1,3 +1,23 @@
 'use strict'
 
-module.exports = { payload }
+const AWS = require('aws-sdk')
+AWS.config.update({region: 'us-west-2'})
+const sns = new AWS.SNS()
+
+const handleMessage = (message) => {
+  const { Message } = JSON.parse(message.Body)
+  const parsedMessage = JSON.parse(Message)
+  console.log(`2. Driver in transit with package for customer ${parsedMessage.customer}`)
+
+  const payload = {
+    Message: Message,
+    TopicArn: 'arn:aws:sns:us-west-2:872892230630:delivered'
+  }
+
+// publish 'delivered' event to SNS
+  sns.publish(payload).promise()
+    .then( () => {console.log('3. Driver delivered package')})
+    .catch(console.error())
+}
+
+module.exports = { handleMessage }
